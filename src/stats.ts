578af78358bddd359b46e99f9f1a5c4abb46ad9c
Ruan Montelo <ruanmontelo@gmail.com>
2020-07-16 14:12:47 -0300

    Using the singleton pattern to make the use of the stats object clearer since only one instance of it should exist in the game at a time

--- src/stats.ts
+++ src/stats.ts
@@ -4,6 +4,16 @@ class Stats {
     hp: number = Config.totalHp;
     gameOver = false;
     score = 0;
+    private static _stats: Stats | undefined;
+
+    private constructor() {  }
+
+    public static getInstance(): Stats {
+        if (!this._stats)
+            this._stats = new Stats();
+
+        return this._stats;
+    } 
 }
 
-export default new Stats();
+export default Stats.getInstance();
